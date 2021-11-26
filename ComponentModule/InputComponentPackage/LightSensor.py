8a5aa116ecc026d2230adb1d22655691c4d82878
kchongee <kchongee@gmail.com>
2021-11-26 19:58:53 +0800

    add update datas to firebase method

--- ComponentModule/InputComponentPackage/LightSensor.py
+++ ComponentModule/InputComponentPackage/LightSensor.py
@@ -3,8 +3,29 @@ from FakeDevices import *
 
 
 class LightSensor(InputComponent):
-    def __init__(self, pin_number):
+    def __init__(self, pin_number):        
         super().__init__(pin_number)
+        self.pin_number = pin_number
 
     def get_light_intensity(self):
         return str(analogRead(self.pin_number))
+    
+    def update(self,db):
+        topic = f"light_{self.pin_number}"
+        subtopics = ["light_intensity"]        
+        subtopics_data = [self.get_light_intensity()]
+        
+        timestamp = round(time.time()*1000)
+        datas = {}
+        
+        if(len(subtopics) == len(subtopics_data)):            
+            for subtopic,subtopic_data in zip(subtopics,subtopics_data):            
+                data = {
+                    f"{subtopic}/{timestamp}": subtopic_data
+                }
+                datas.update(data)                
+        else:
+            raise Exception("The subtopics and subtopics_data must have same length!")
+            
+        print(datas)                            
+        db.child(topic).update(datas)
--- Main.py
+++ Main.py
@@ -1,7 +1,25 @@
 from ComponentModule.InputComponentPackage.UltrasonicSensor import *
 from ComponentModule.InputComponentPackage.LightSensor import *
 from time import *
+from pyrebase import pyrebase
 
+config = {
+    "apiKey":"AIzaSyDGGWQfuvSiiCPpL2MUIJi1HO_TdmscVlY",
+    "authDomain":"bait2123-iot-b0887.firebaseapp.com",
+    "databaseURL":"https://bait2123-iot-b0887-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "storageBucket":"gs://bait2123-iot-b0887.appspot.com",
+}
+
+try:
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password("kchongee@gmail.com","zt/h!\!*B;{)8/U$")
+    db = firebase.database()
+    storage = firebase.storage()
+except Exception as e:
+    print(e)
+    exit(-1)
+    
 
 def main():
     # region : sensor declaration section
@@ -11,13 +29,14 @@ def main():
 
     while True:
         try:
-            print(ultrasonic_sensor.get_distance())
             print(light_sensor.get_light_intensity())
-            sleep(1)
+            print(ultrasonic_sensor.get_distance())
+            light_sensor.update(db)
+            ultrasonic_sensor.update(db)
+            sleep(2)
 
         except KeyboardInterrupt:
             break
-
         except TypeError:
             print("Type Error occurs")
             break
